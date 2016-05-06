b503823c0226a515c620d9dac00801a7f3a93d52
alitalpur99 <alitalpur99@gmail.com>
2016-05-06 10:29:07 -0400

    now Auto adding flows

--- singleswitch-mptcp.py
+++ singleswitch-mptcp.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
 """ Script to use MPTCP in Mininet. Run on commandline as, python singleswitch-mptcp.py
-    Start the switch s1 in mininet, "switch s1 start" command.
-    add flows in s1 by :ovs-ofctl add-flow s1 in_port=1,actions=output:3 or drop ...and so on
+    # auto done, no need to do it {Start the switch s1 in mininet, "switch s1 start" command.
+    (add flows in s1 by :ovs-ofctl add-flow s1 in_port=1,actions=output:3 or drop ...and so on)}
     check the iperf for multipath connection (MPTCP) gained throughput over two links!!!
     links could be set down : link s1 h2 down
     or simply by droping the returning packets
@@ -17,7 +17,7 @@ from mininet.link import Link, TCLink
 def topology():
     "Create a network."
   # Defining network 
-    net = Mininet( controller=None, link=TCLink, switch=OVSKernelSwitch )
+    net = Mininet( controller=RemoteController, link=TCLink, switch=OVSKernelSwitch )
     print "*** Creating nodes"
   # Adding hosts, switches and controller
     h1 = net.addHost( 'h1', mac='00:00:00:00:00:01', ip='10.0.0.1/24' )
@@ -26,13 +26,7 @@ def topology():
     #s2 = net.addSwitch( 's2' )
     #c0 = net.addController( 'c0', controller=RemoteController, ip='127.0.0.1', port=6653 )
     
-    print "*** Creating links"
-
-  # net.addLink(s1, h2, 2, 0)
-  # net.addLink(h1, s1, 0, 1)
-  # net.addLink(s2, h2, 2, 0)
-  # net.addLink(h1, s2, 0, 1)
-  
+    print "*** Creating links"  
   # Adding links and link bandwidth
     net.addLink(h1, s1, intfName1='h1-eth0', intfName2='s1-eth1', bw=10)
     net.addLink(h1, s1, intfName1='h1-eth1', intfName2='s1-eth2', bw=10)
@@ -46,9 +40,15 @@ def topology():
 
     print "*** Starting network"
     net.build()
-   # c0.start()
-   # s1.start()
-  #  s2.start( [c0] )
+   # start s1 switch
+    s1.start('')
+    s1.cmd('switch s1 start')
+   # add flows in switch
+    s1.cmd('ovs-ofctl add-flow s1 in_port=1,actions:output=3')
+    s1.cmd('ovs-ofctl add-flow s1 in_port=2,actions:output=4')
+    s1.cmd('ovs-ofctl add-flow s1 in_port=3,actions:output=1')
+    s1.cmd('ovs-ofctl add-flow s1 in_port=4,actions:output=2')
+
 
     print "*** Running CLI"
     CLI( net )
