5463083a34145a71ba3b57aba01a2a60b6eb3a5e
alitalpur99 <alitalpur99@gmail.com>
2016-10-14 20:37:28 +0200

    Update h1.sh

--- Experiment/h1.sh
+++ Experiment/h1.sh
@@ -4,8 +4,8 @@ tcpdump -i h1-eth0 -w h1 &
 
 > /var/log/syslog
 
-ovs-ofctl snoop s1 --timestamps 2> switch1 &
-ovs-ofctl snoop s2 --timestamps 2> switch2 &
-#ovs-ofctl snoop s4 --timestamps 2> switch4 &
+ovs-ofctl snoop s1 --timestamp 2> switch1 &
+ovs-ofctl snoop s2 --timestamp 2> switch2 &
+#ovs-ofctl snoop s4 --timestamp 2> switch4 &
 
 iperf3 -c 10.0.0.4 -B 10.0.0.1 --cport 55000 -t50 -i1 > iperf3h1.txt
