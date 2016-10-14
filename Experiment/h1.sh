fba8b85ab150ccdc62ebb72075afe84bf3ad2934
alitalpur99 <alitalpur99@gmail.com>
2016-10-14 19:37:18 +0200

    add snoop command

--- Experiment/h1.sh
+++ Experiment/h1.sh
@@ -2,4 +2,10 @@
 
 tcpdump -i h1-eth0 -w h1 &
 
-iperf3 -c 10.0.0.4 -B 10.0.0.1 --cport 55000 -t100 -i1 > iperf3h1.txt
+> /var/log/syslog
+
+ovs-ofctl snoop s1 --timestamps 2> switch1 &
+ovs-ofctl snoop s2 --timestamps 2> switch2 &
+#ovs-ofctl snoop s4 --timestamps 2> switch4 &
+
+iperf3 -c 10.0.0.4 -B 10.0.0.1 --cport 55000 -t50 -i1 > iperf3h1.txt
