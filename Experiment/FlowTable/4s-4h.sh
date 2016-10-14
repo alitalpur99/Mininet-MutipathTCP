341affae0f3a076a61f7807e47b84cbed6798d91
alitalpur99 <alitalpur99@gmail.com>
2016-10-14 19:30:44 +0200

    added src h4 in s3

--- Experiment/FlowTable/4s-4h.sh
+++ Experiment/FlowTable/4s-4h.sh
@@ -40,6 +40,9 @@ sh ovs-ofctl add-flow s3 dl_src=00:00:00:00:00:01,dl_dst=00:00:00:00:00:04,actio
 sh ovs-ofctl add-flow s3 dl_src=00:00:00:00:00:02,dl_dst=00:00:00:00:00:03,actions=output:1
 sh ovs-ofctl add-flow s3 dl_src=00:00:00:00:00:02,dl_dst=00:00:00:00:00:04,actions=output:3
 
+sh ovs-ofctl add-flow s3 dl_src=00:00:00:00:00:04,dl_dst=00:00:00:00:00:01,actions=output:2
+sh ovs-ofctl add-flow s3 dl_src=00:00:00:00:00:04,dl_dst=00:00:00:00:00:02,actions=output:2
+sh ovs-ofctl add-flow s3 dl_src=00:00:00:00:00:04,dl_dst=00:00:00:00:00:03,actions=output:1
 
 
 sh ovs-ofctl add-flow s4 dl_type=0x806,nw_proto=1,action=flood
